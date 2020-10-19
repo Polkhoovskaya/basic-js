28a3b02bec00aa558ea2ee2492322b6a3b21a629
Palkhouskaya <vpalkhouskaya@scnsoft.com>
2020-10-19 11:22:39 +0300

    add: haroi-tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,5 +1,7 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
-  
+    let turns = Math.pow(2, disksNumber) - 1;
+    let seconds = Math.floor(turns / turnsSpeed * 3600)
+    return { turns: turns, seconds: seconds };
 };
